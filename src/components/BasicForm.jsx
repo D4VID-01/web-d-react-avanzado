f2f26386d316b43b2876abb44f32b9da27017341
Luis David <ld0110@gmail.com>
2025-07-10 19:11:44 -0600

    feat: esquema de validación con yup

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -1,7 +1,28 @@
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+/* Esquema para validación */
+const schema = yup.object({
+  username: yup
+    .string()
+    .required('El nombre es obligatorio'),
+  password: yup
+    .string()
+    .min(6, 'La contaseña debe tener minimo 6 caracteres')
+    .required('La contraseña es obligatoria'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Las contraseñas no coinciden')
+    .required('La contraseña es obligatoria')
+})
 
 export const BasicForm = () => {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid }
+  } = useForm({})
 
   const onSubmit = (data) => {
     console.log(data)
