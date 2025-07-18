90d02f1518ff33d8f60f68ca236a176d069209eb
luisDavid <ld0110mp@gmail.com>
2025-07-17 18:09:02 -0600

    feat: Creación contexto global ChatContext

---
+++ src/context/ChatContext.jsx
@@ -0,0 +1,41 @@
+import React, { createContext, useReducer } from 'react'
+
+// 1. Crear el contexto global
+export const ChatContext = createContext()
+
+// Paso 1 (parte-3): Crear el estado inicial
+const initialState = {
+  messages: []
+}
+
+// Paso 2 (parte-3): Crear la función reductora
+const chatReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      console.log('agregando mensaje...')
+      console.log(state)
+      return { ...state, messages: [...state.messages, action.payload] }
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload }
+    default:
+      return state
+  }
+}
+
+/* // Paso 4 (parte-3): Generar los dispatch
+// Dispatch para guardar el mensaje del usuario
+dispatch({ type: 'ADD_MESSAGE', payload: { form: 'user', text: userPrompt } })
+// Dispatch para guardar el mensaje del bot
+dispatch({ type: 'ADD_MESSAGE', payload: { form: 'bot', text: res.data.response } }) */
+
+// 2. Provider
+export const ChatProvider = ({ children }) => {
+// Paso 3 (parte-3): uso de hook useReducer
+  const [state, dispatch] = useReducer(chatReducer, initialState)
+
+  return (
+    <ChatContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
+  )
+}
--- src/hooks/useOllama.js
+++ src/hooks/useOllama.js
@@ -1,27 +1,6 @@
 import axios from 'axios'
-import { useReducer } from 'react'
-
-// Paso 1 (parte-3): Crear el estado inicial
-const initialState = {
-  messages: []
-}
-
-// Paso 2 (parte-3): Crear la función reductora
-const chatReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_MESSAGE':
-      console.log('agregando mensaje...')
-      console.log(state)
-      return { ...state, messages: [...state.messages, action.payload] }
-    default:
-      return state
-  }
-}
 
 export const useOllama = () => {
-  // Paso 3 (parte-3): uso de hook useReducer
-  const [dispatch] = useReducer(chatReducer, initialState)
-
   const sendMessage = async (userPrompt) => {
     try {
       const res = await axios.post('http://localhost:11434/api/generate', {
@@ -29,11 +8,7 @@ export const useOllama = () => {
         prompt: userPrompt,
         stream: false
       })
-      // Paso 4 (parte-3): Generar los dispatch
-      // Dispatch para guardar el mensaje del usuario
-      dispatch({ type: 'ADD_MESSAGE', payload: { form: 'user', text: userPrompt } })
-      // Dispatch para guardar el mensaje del bot
-      dispatch({ type: 'ADD_MESSAGE', payload: { form: 'bot', text: res.data.response } })
+      return res
     } catch (error) {
       console.error('error: ', error)
     }
