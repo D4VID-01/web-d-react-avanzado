6b466a2a7d5963e128b163d5aea5f7115bad7bfb
luisDavid <ld0110mp@gmail.com>
2025-07-22 21:03:07 -0600

    feat: Agrega endpoint POST con Express.js

--- express/src/app.js
+++ express/src/app.js
@@ -49,6 +49,18 @@ app.get('/api/peliculas/comedia', (req, res) => {
   res.send(infoPeliculas.comedia)
 })
 
+// Método POST
+app.use(express.json())
+app.post('/api/peliculas', (req, res) => {
+  const nuevaPelicula = req.body
+
+  console.log(nuevaPelicula)
+  res.status(201).send({
+    mensaje: 'La película se recibió con éxito',
+    datos: nuevaPelicula
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
