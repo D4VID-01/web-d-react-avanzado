eeb42bf95b1b8b273da978f7ffcd3dadbbe76702
luisDavid <ld0110mp@gmail.com>
2025-07-22 20:43:51 -0600

    feat: Agrega endpoints GET con Express.js

---
+++ express/src/peliculas.js
@@ -0,0 +1,45 @@
+const infoPeliculas = {
+  accion: [
+    {
+      id: 1,
+      titulo: 'Misión imposible',
+      year: 2015,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'Mad Max: Furia en el camino',
+      year: 2015,
+      pais: 'australia'
+    },
+    {
+      id: 3,
+      titulo: 'John Wick',
+      year: 2014,
+      pais: 'usa'
+    }
+  ],
+  comedia: [
+    {
+      id: 1,
+      titulo: 'Supercool',
+      year: 2007,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'La Gran Aventura Lego',
+      year: 2014,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: '¿Qué pasó ayer?',
+      year: 2009,
+      pais: 'usa'
+    }
+  ]
+
+}
+
+module.exports.infoPeliculas = infoPeliculas
