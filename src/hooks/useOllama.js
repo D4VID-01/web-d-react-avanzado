e735b7acea8d10138aa6b161376eaa538a0c536d
Luis David <ld0110mp@gmail.com>
2025-07-29 19:51:05 -0600

    feat: Agraga endpoint con Ollama con custome-hook

--- src/hooks/useOllama.js
+++ src/hooks/useOllama.js
@@ -1,17 +1,9 @@
 import axios from 'axios'
 
 export const useOllama = () => {
-  const sendMessage = async (userPrompt) => {
-    try {
-      const res = await axios.post('http://localhost:11434/api/generate', {
-        model: 'llama2',
-        prompt: userPrompt,
-        stream: false
-      })
-      return res
-    } catch (error) {
-      console.error('error: ', error)
-    }
+  const sendMessage = async (prompt) => {
+    const res = await axios.post('http://localhost:3001/api/chat', { prompt })
+    return res
   }
 
   return { sendMessage }
